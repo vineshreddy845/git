5dd266022985c43a5ca19000b6b290b8a42357af
vineshreddy845 <64718480+vineshreddy845@users.noreply.github.com>
2022-06-21 21:15:40 -0500

    added int and string datatypes

--- basic.java
+++ basic.java
@@ -1,5 +1,8 @@
 public class basic {
     public static void main(String args[]) {
-        System.out.println("hello");
+        System.out.println("hi vinesh");
+        int a = 10;
+        String name = "vinesh";
+        System.out.println("a=" + a + "first=" + name);
     }
 }
